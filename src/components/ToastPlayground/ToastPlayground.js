84fd79933c40746ec0b701c70d3e5999e06223f7
Riza Nugraha <rnugraha@gmail.com>
2025-02-26 22:42:18 +0100

    excercise 2
    - The toast component should show the message entered in the textarea, essentially acting as a “live preview”.
    - The toast's styling should be affected by the “variant” selected:
    - The colors can be set by specifying the appropriate class on the top-level <div>. By default, it's set to styles.notice, but you'll want to dynamically select the class based on the variant (eg. for a success toast, you'll want to apply styles.success).
    - The icon can be selected from the ICONS_BY_VARIANT object. Feel free to re-organize things however you wish!
    - The toast should be hidden by default, but can be shown by clicking the "Pop Toast!” button.
    - The toast can be hidden by clicking the “×” button within the toast.

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -3,12 +3,18 @@ import React from "react";
 import Button from "../Button";
 
 import styles from "./ToastPlayground.module.css";
+import Toast from "../Toast/Toast";
 
 const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
   const [message, setMessage] = React.useState("");
   const [variant, setVariant] = React.useState("notice");
+  const [isToastVisible, setIsToastVisible] = React.useState(false);
+
+  handleClick = () => {
+    setIsToastVisible(true);
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -16,6 +22,14 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+      {isToastVisible && (
+        <Toast
+          message={message}
+          variant={variant}
+          visibility={isToastVisible}
+          onClose={() => setIsToastVisible(false)}
+        />
+      )}
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -59,7 +73,7 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <Button onClick={handleClick}>Pop Toast!</Button>
           </div>
         </div>
       </div>
